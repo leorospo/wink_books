e64b341ce3c2531e876c9f78f23e791812e75fa0
Leonardo Mancori <leonardo.mancori@gmail.com>
2019-05-03 12:53:51 +0200

    Search bar implemented @TODO: paginate results

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './App.css';
-import Books from './components/Books'
+import Books from './components/Books';
+import SearchBar from './components/SearchBar'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            books: []
         }
     }
+
+    getBooks = query => fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    printBooks = query => this.getBooks(query)
+        .then(r => r.json())
+        .then(r => r.items)
+        .then(r =>
+            this.setState({
+                books: r
+            })
+        )
 
     render() {
         return (
             <div className="">
-                <Books />
+                <SearchBar printBooks={this.printBooks} />
+                <Books books={this.state.books} />
             </div>
         )
     }
--- src/components/Books.js
+++ src/components/Books.js
@@ -4,27 +4,13 @@ export default class Books extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            books: []
         }
     }
-
-    getBooks = query => fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
-    printBooks = query => this.getBooks(query)
-        .then(r => r.json())
-        .then(r => r.items)
-        .then(r =>
-            this.setState({
-                books: r
-            })
-        )
 
     render() {
         return (
             <div>
-                {
-                    this.state.books.map(el => <p key={el.id}>{el.volumeInfo.title}</p>)
-                }
-                <button onClick={() => this.printBooks('a')}>click</button>
+                {this.props.books.map(el => <p key={el.id}>{el.volumeInfo.title}</p>)}
             </div>
         )
     }
