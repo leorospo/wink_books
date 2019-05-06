e725a9bd9de8b395ae0947f2b73fb73c3ffa356b
Leonardo Mancori <leonardo.mancori@gmail.com>
2019-05-06 10:29:48 +0200

    first styling edits

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,15 +25,18 @@ export default class SearchBar extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
+            <form className='search-bar' onSubmit={this.onSubmit}>
                 <label>Search in Google Books:</label>
-                <input
-                    type='text'
-                    value={this.state.searchTerm}
-                    onChange={this.searchTermHandler}
-                    placeholder='Type something...'
-                />
-                <button type='submit'>Search</button>
+                <div className='search-bar-wrapper'>
+                    <input
+                        class='search-input'
+                        type='text'
+                        value={this.state.searchTerm}
+                        onChange={this.searchTermHandler}
+                        placeholder='Type something...'
+                    />
+                    <button class='search-btn' type='submit'>Search</button>
+                </div>
             </form>
 
         )
